b1014b86110fcdc655386dc5bfc6a7412ac8d6e6
Mouad <mouadlouhibi2000@gmail.com>
2024-02-02 13:00:40 +0100

    Service

---
+++ src/app/service/Service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Model } from '../model/Model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Service {
+
+  public models: Array<Model>
+  private url: string = "http://localhost:8080/students";
+
+  constructor(private httpClient: HttpClient) {
+    this.models = new Array<Model>();
+    // GET Models
+    this.httpClient.get<Array<Model>>(this.url)
+      .subscribe(
+        (res) => { this.models.push(...res) },
+        (err) => { console.log(err) }
+      );
+  }
+}
