4f2603a61b8fdf5249bd6a445f8c6836be71fb9f
Mouad <mouadlouhibi2000@gmail.com>
2024-02-06 00:38:13 +0100

    Add Student Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BodyComponent } from './component/body/body.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { AddStudentComponent } from './add-student/add-student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BodyComponent
+    BodyComponent,
+    AddStudentComponent,
   ],
   imports: [
     BrowserModule,
