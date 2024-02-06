abeaeeea0b7f9a4f8d020aab9be16e74cdcb220a
Mouad <mouadlouhibi2000@gmail.com>
2024-02-07 00:45:20 +0100

    Add Student

--- src/app/component/add-student/add-student.component.ts
+++ src/app/component/add-student/add-student.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Model } from '../../model/Model';
+import { Service } from '../../service/Service';
 
 @Component({
   selector: 'app-add-student',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './add-student.component.css'
 })
 export class AddStudentComponent {
+
+  public form: any;
+  public models: Array<Model>
+
+  constructor(private service: Service) {
+    this.models = this.service.getModels();
+  }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      sujet: new FormControl(''),
+      description: new FormControl(''),
+      chef: new FormControl(''),
+      chercheur: new FormControl(''),
+    })
+  }
 
 }
