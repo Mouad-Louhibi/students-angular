0e8e0abb792c172891edf3af75f2aa53f0e5731d
Mouad <mouadlouhibi2000@gmail.com>
2024-02-18 16:40:05 +0100

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddStudentComponent } from './component/add-student/add-student.component';
+import { ListStudentsComponent } from './component/list-students/list-students.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add-student', component: AddStudentComponent },
+  { path: 'list-students', component: ListStudentsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
